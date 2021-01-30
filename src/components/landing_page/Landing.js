76f31d7238d237e8061e410e41874f4d40f572e6
ShatteredIce <icywind.katsuo@gmail.com>
2021-01-30 16:51:39 -0600

    final ui tweaks on main page

--- src/components/landing_page/Landing.js
+++ src/components/landing_page/Landing.js
@@ -8,16 +8,16 @@ function Landing() {
       <div className="innerbox">
         <header>
           <h1 className="white">Experience</h1>
-          <h1 className="pink">Low Latency</h1>
+          <h1 className="pink">Low-Latency</h1>
         </header>
         <p className="text">{desc}</p>
         <div>
-          <Link to='/demo' className="text pink" id="leftbutton">{"Asciify Yourself >"}</Link>
+          <Link to='/demo' className="text pink" id="leftbutton">{"Asciifi Yourself >"}</Link>
           <Link to='/video-call' className="text pink" id="rightbutton">{"Call a friend >"}</Link>
         </div>
 
       </div>
-      
+
     </div>
   )
 }
