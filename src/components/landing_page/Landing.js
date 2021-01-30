62294ee08ca0ff59ba1696c3fe55d47d1c580008
joshuajung01 <tojoshuajung@gmail.com>
2021-01-30 15:51:13 -0600

    centering text

--- src/components/landing_page/Landing.js
+++ src/components/landing_page/Landing.js
@@ -5,13 +5,18 @@ function Landing() {
   const desc = "A fast P2P video-calling platform.\n No sign up required."
   return (
          <body>
-          <header>
-           <h1 className="white">Experience</h1>
-           <h1 className="pink">Low Latency</h1>
-          </header>
-          <p className="text">{desc}</p>
-          <a className="text pink" href="https://github.com/Abhishek-More" id="leftbutton">{"Asciify Yourself >"}</a>
-          <a className="text pink" href="https://github.com/Abhishek-More" id="rightbutton">{"Call a friend >"}</a>
+           <div className="innerbox">
+              <header>
+                <h1 className="white">Experience</h1>
+                <h1 className="pink">Low Latency</h1>
+              </header>
+              <p className="text">{desc}</p>
+              <div>
+                <a className="text pink" href="https://github.com/Abhishek-More" id="leftbutton">{"Asciify Yourself >"}</a>
+                <a className="text pink" href="https://github.com/Abhishek-More" id="rightbutton">{"Call a friend >"}</a>
+              </div>
+           </div>
+          
         </body>
     )
 
