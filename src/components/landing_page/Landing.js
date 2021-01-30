b6844ebab7957e14945570923538e85b1edd7296
joshuajung01 <tojoshuajung@gmail.com>
2021-01-30 16:25:10 -0600

    spacing corrections

--- src/components/landing_page/Landing.js
+++ src/components/landing_page/Landing.js
@@ -4,14 +4,20 @@ function Landing() {
 
   const desc = "A fast P2P video-calling platform.\n No sign up required."
   return (
-    <div>
-      <header>
-        <h1 className="white">Experience</h1>
-        <h1 className="pink">Low Latency</h1>
-      </header>
-      <p className="text">{desc}</p>
-      <Link to='/demo' className="text pink" id="leftbutton">{"Asciify Yourself >"}</Link>
-      <Link to='/video-call' className="text pink" id="rightbutton">{"Call a friend >"}</Link>
+    <div className="body">
+      <div className="innerbox">
+        <header>
+          <h1 className="white">Experience</h1>
+          <h1 className="pink">Low Latency</h1>
+        </header>
+        <p className="text">{desc}</p>
+        <div>
+          <Link to='/demo' className="text pink" id="leftbutton">{"Asciify Yourself >"}</Link>
+          <Link to='/video-call' className="text pink" id="rightbutton">{"Call a friend >"}</Link>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
