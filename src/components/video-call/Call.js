6eb98fb5dabdc357a32cd79a360d6f5a4c2efab5
Abhishek <abhimore74@gmail.com>
2021-01-31 05:45:32 -0500

    ui

--- src/components/video-call/Call.js
+++ src/components/video-call/Call.js
@@ -4,32 +4,32 @@ import "./Call.css";
 function Call() {
 
     useEffect(() => {
+
+
         const script = document.createElement("script");
         script.src = "peersend.js";
         document.body.appendChild(script);
 
         return () => {
             script.remove();
-            //const str = video.srcObject;
-            //str.getVideoTracks().forEach(function (track) {
-            //    track.stop();
-            //});
+
         };
     });
 
     return (
         <div>
+            <p id="id-holder">Your ID: Loading</p>
             <div className='video-pair'>
                 <div />
-                <p id="disp">Self</p>
-                <p id='remote'>Remote</p>
-                <div /></div>
-            <input id='id'></input>
-            <p id="id-holder">Your ID: Loading</p>
-
-            <a id="clicker">
-                Connect To Peer
-            </a>
+                <p id="disp"></p>
+                <p id='remote'>?</p>
+                <div />
+            </div>
+            <div className='io'>
+            
+                <input id='id'></input>
+                <a id="clicker">Connect To Peer</a>
+            </div>
 
         </div>
     );
